7a2a2cd5c31f4ec529ac7a091199f43b2dcf3495
kurt77 <mattoni@gmail.com>
2018-12-11 10:48:36 +0100

    Create most_popular_sport.js

---
+++ most_popular_sport.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var Alexa = require("alexa-sdk");
+
+var handlers = {
+  'LaunchRequest': function() {
+    this.respose.speak("Hello, what do you think is the world's most popular sport?").listen("Tell me what you think is the world's most popular sport");
+    this.emit(':responseReady');
+    
+  },
+
+  'MostPopularSportIntent': function () {
+    var worldSport = this.event.request.intent.slots.sport.value;
+    if(worldSport === "soccer"){
+      this.emit(:tell,"Correct! Soccer is the world's most popular sport.");
+      
+    }else{
+      this.emit(:tell,"You guessed that " + worldSport + "is the most popular.. Actually, soccer is the world's most popular sport")
+      
+    }
+  },
+}
+
+exports.handler = function(event, context, callback){
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
